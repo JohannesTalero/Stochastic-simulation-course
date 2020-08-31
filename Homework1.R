7f0f774ad31c850db76d3afc492eb47947a74404
johannesta <johannes.talero@gmail.com>
2020-08-31 00:38:12 -0500

    Homework 1 2a

--- Homework1.R
+++ Homework1.R
@@ -1,10 +1,10 @@
-##################### STOCHASTIC SIMULATION #####################
+##################### STOCHASTIC SIMULATION ####################
 #####################  HOMEWORK 1 ##############################
 #Johannes Talero
 library(ggplot2)
 
 #2. Inverse method for discrete r.v.'s (Jones):
-#  Consider the discrete random variable (?.v.) X with probability mass function (pmf ) given by:
+#  Consider the discrete random variable (r?v.) X with probability mass function (pmf ) given by:
 #  P(X = ???1) = 0.2, P(X = 0) = 0.5, P(X = 1) = 0.3
 
 #a) Calculate and plot the Cumulative Distribution Function (CDF) F_X(x) of X.
